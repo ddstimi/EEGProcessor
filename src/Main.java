66eb1e0cb739ea8305a4fa6c1c56e9306600b866
ddstimi <h257330@stud.u-szeged.hu>
2025-08-04 22:26:19 +0200

    feat: open file manager and create main to run app

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import gui.MainWindow;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainWindow window = new MainWindow();
+            window.setVisible(true);
+        });
+    }
+}
--- src/gui/ControlPanel.java
+++ src/gui/ControlPanel.java
@@ -1,17 +1,28 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
-public class ControlPanel extends  JFrame{
-
-    private JPanel contentPane;
-    private JFileChooser fileChooser;
-
+public class ControlPanel {
+    private JPanel panel;
     private JButton openButton;
     private JButton startButton;
     private JButton pauseButton;
-
     private JButton stopButton;
+    private JFileChooser fileChooser;
+
+    public ControlPanel() {
+        fileChooser = new JFileChooser();
+        openButton.addActionListener(this::handleOpenButton);
+    }
+
+    private void handleOpenButton(ActionEvent e) {
+        if (fileChooser.showOpenDialog(panel) == JFileChooser.APPROVE_OPTION) {
+            System.out.println("Selected: " + fileChooser.getSelectedFile());
+        }
+    }
+
+    public JPanel getMainPanel() {
+        return panel;
+    }
 }
-
-
