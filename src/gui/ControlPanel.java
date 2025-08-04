c308bd4c8271943b26be6b2ef1ed650c2ad44302
ddstimi <h257330@stud.u-szeged.hu>
2025-08-04 21:42:53 +0200

    chore: initialize project and create gui base

---
+++ src/gui/ControlPanel.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javax.swing.*;
+
+public class ControlPanel extends  JFrame{
+
+    private JPanel contentPane;
+    private JFileChooser fileChooser;
+
+    private JButton openButton;
+    private JButton startButton;
+    private JButton pauseButton;
+
+    private JButton stopButton;
+}
+
+
