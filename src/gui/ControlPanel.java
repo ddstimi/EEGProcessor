736a4fe8fcc218adda4eb53010180aafcd2d9450
ddstimi <h257330@stud.u-szeged.hu>
2025-08-05 13:18:02 +0200

    feat: separate ControlPanel class to use new controller package

--- src/gui/ControlPanel.java
+++ src/gui/ControlPanel.java
@@ -1,7 +1,10 @@
 package gui;
 
+import controller.EEGController;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class ControlPanel {
     private JPanel panel;
@@ -11,14 +14,19 @@ public class ControlPanel {
     private JButton stopButton;
     private JFileChooser fileChooser;
 
+    private final EEGController controller;
+
     public ControlPanel() {
+        this.controller = new EEGController();
         fileChooser = new JFileChooser();
+
         openButton.addActionListener(this::handleOpenButton);
     }
 
     private void handleOpenButton(ActionEvent e) {
         if (fileChooser.showOpenDialog(panel) == JFileChooser.APPROVE_OPTION) {
-            System.out.println("Selected: " + fileChooser.getSelectedFile());
+            File file = fileChooser.getSelectedFile();
+            controller.setSelectedFile(file);
         }
     }
 
