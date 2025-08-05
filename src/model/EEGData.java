e4cdaab8b4a2a7fa227234a456bc836e23b15f6a
ddstimi <h257330@stud.u-szeged.hu>
2025-08-05 17:46:18 +0200

    chore: implement sliding average

--- src/model/EEGData.java
+++ src/model/EEGData.java
@@ -4,17 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EEGData {
-    private final List<Integer> samples = new ArrayList<>();
+    private final List<List<Integer>> channels;
 
-    public void addSample(int sample) {
-        samples.add(sample);
+    public EEGData() {
+        channels = new ArrayList<>();
+        for (int i = 0; i < 32; i++) {
+            channels.add(new ArrayList<>());
+        }
     }
 
-    public int getSampleCount() {
-        return samples.size();
+    public void addSampleToChannel(int channelIndex, int sample) {
+        if (channelIndex < 1 || channelIndex > 32) {
+            throw new IllegalArgumentException("Invalid channel index: " + channelIndex);
+        }
+        channels.get(channelIndex - 1).add(sample);
     }
 
-    public List<Integer> getSamples() {
-        return samples;
+
+    public List<Integer> getChannelSamples(int channelIndex) {
+        if (channelIndex < 1 || channelIndex > 32) {
+            throw new IllegalArgumentException("Invalid channel index: " + channelIndex);
+        }
+        return channels.get(channelIndex - 1);
+    }
+
+    public int getChannelCount() {
+        return channels.size();
+    }
+
+    public int getSampleCountPerChannel() {
+        return channels.get(0).size();
     }
 }
